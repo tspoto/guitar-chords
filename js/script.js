d0264dc7bec7e3afe546c826106812c4b9ffdf87
Tony Spoto <tonyxspote@gmail.com>
2023-11-20 15:52:47 -0500

    Avoid repeated cards & add space bar trigger

--- js/script.js
+++ js/script.js
@@ -1,33 +1,44 @@
 fetch('js/chords.json')
 .then(response => response.json())
 .then(data => {
-var cards = data;
-var current_card_index = 0;
+ var cards = data;
+ var last_card = null;
+ var current_card_index = 0;
 
-function shuffle_array(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-}
+ function shuffle_array(array) {
+ for (let i = array.length - 1; i > 0; i--) {
+  const j = Math.floor(Math.random() * (i + 1));
+  [array[i], array[j]] = [array[j], array[i]];
+ }
+ }
 
-function display_chord() {
-  var card_content = document.getElementById('card_content');
-  fetch(cards[current_card_index].chord)
-   .then(response => response.text())
-   .then(data => {
-     card_content.innerHTML = data;
-   })
-   .catch(error => console.error('Error:', error));
-}
+ function display_chord() {
+ var card_content = document.getElementById('card_content');
+ fetch(cards[current_card_index].chord)
+ .then(response => response.text())
+ .then(data => {
+  card_content.innerHTML = data;
+  last_card = cards[current_card_index];
+ })
+ .catch(error => console.error('Error:', error));
+ }
 
-document.getElementById('next_button').addEventListener('click', function() {
-    shuffle_array(cards);
-    current_card_index = 0;
-    display_chord();
-});
-   
-shuffle_array(cards);
-display_chord();
+ function next_card() {
+ do {
+  shuffle_array(cards);
+  current_card_index = 0;
+ } while (cards[current_card_index] === last_card);
+ display_chord();
+ }
+
+ document.getElementById('next_button').addEventListener('click', next_card);
+ window.addEventListener('keydown', function(event) {
+ if (event.code === 'Space') {
+  next_card();
+ }
+ });
+
+ shuffle_array(cards);
+ display_chord();
 })
 .catch(error => console.error('Error:', error));
